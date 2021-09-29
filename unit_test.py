7f4c9fb33e2b97e02668113ffcb67bc0a8fd64cf
shots47s <stbrown@psc.edu>
2021-09-29 08:09:52 -0400

    Added unittest class

--- unit_test.py
+++ unit_test.py
@@ -1,11 +1,26 @@
+"""
+Example code for unit_testing methods
+"""
 import unittest
 
+
 class T:
+    """
+    Just a class to make something different
+    """
+
     def __init__(self):
+        """
+        Instance Creation Operator
+        """
         self.x = 1.000
 
 
 def add_x_and_k(x, k):
+    """
+    Simple function that checks types
+    """
+
     if isinstance(x, str):
         return x + str(k)
     elif isinstance(x, int):
@@ -17,7 +32,14 @@ def add_x_and_k(x, k):
 
 
 class TestAddXAndK(unittest.TestCase):
+    """
+    A unitest module test case for the module
+    """
+
     def test_add(self):
+        """
+        Testing the add function
+        """
         self.assertEqual(add_x_and_k("x", "0.001"), "x0.001")
         self.assertEqual(add_x_and_k(1, 1.022), 2)
         self.assertAlmostEqual(add_x_and_k(1.01, 2.01), 3.02)
@@ -27,27 +49,5 @@ class TestAddXAndK(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    # Unit testing for module
     unittest.main()
-
-    """
-    ### Unit testing for module
-
-    pass_flag = True
-    if add_x_and_k("x", "0.001") != "x0.001":
-        print("string failed")
-        pass_flag = False
-    if add_x_and_k(1, 1.022) != 2:
-        print("int failed")
-        pass_flag = False
-    if add_x_and_k(1.01, 2.01) != 1.01 + 2.01:
-        print("float failed")
-        pass_flag = False
-    if add_x_and_k(True, 1.020) != "this would be an error":
-        print("error failed")
-        pass_flag = False
-
-    if pass_flag:
-        print("All Tests Passed")
-    else:
-        print("Some Tests Failed")
-    """
